7d86303652891013d49edf91527b6dc971088c3d
Ahsan Rehmani <rehmaniahsan15@gmail.com>
2023-05-27 18:01:08 +0500

    Added factory and seeder for fake attendance record

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -3,17 +3,16 @@
 namespace Database\Seeders;
 
 use App\Models\Attendance;
-use App\Models\User;
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class AttendanceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $user = User::factory()->count(15)->create();
+        Attendance::factory()->count(25)->create();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            UserSeeder::class
+            UserSeeder::class,
+            AttendanceSeeder::class
         ]);
         // \App\Models\User::factory(10)->create();
 
